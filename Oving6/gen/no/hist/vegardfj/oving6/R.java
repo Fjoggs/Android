73588d3f97d60e530b61db91457efca6d31e49aa
Fjoggis <fjoggis@gmail.com>
2012-11-03 19:25:07 +0100

    Made 3 classes, but they don't work properly against each other.

--- Oving6/gen/no/hist/vegardfj/oving6/R.java
+++ Oving6/gen/no/hist/vegardfj/oving6/R.java
@@ -34,8 +34,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
-        public static final int login_button_logIn=0x7f040006;
-        public static final int login_button_restart=0x7f040007;
+        public static final int login_button_send=0x7f040006;
+        public static final int login_button_tryAgain=0x7f040007;
         public static final int login_textView_cardnumber=0x7f040005;
         public static final int login_textView_name=0x7f040004;
         public static final int menu_settings=0x7f040002;
--- Oving6/src/no/hist/vegardfj/oving6/MainActivity.java
+++ Oving6/src/no/hist/vegardfj/oving6/MainActivity.java
@@ -1,10 +1,16 @@
 package no.hist.vegardfj.oving6;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends FragmentActivity {
+	LoginFragment loginFragment = new LoginFragment();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,9 @@ public class MainActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_ta_shansen, menu);
         return true;
+    }
+    
+    public void onClickLogIn(View v) throws ClientProtocolException, IOException {
+    	loginFragment.tryLogIn("Tomas", "123");
     }
 }
