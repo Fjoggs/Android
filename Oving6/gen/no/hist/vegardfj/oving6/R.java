87b66397c53d5b3fa319f645b5e5b31ac9cf86b4
Fjoggis <fjoggis@gmail.com>
2012-11-05 23:34:14 +0100

    Using the provided HttpActivity and wrapperclass instead. Pre-delete of my own version.

--- Oving6/gen/no/hist/vegardfj/oving6/R.java
+++ Oving6/gen/no/hist/vegardfj/oving6/R.java
@@ -15,13 +15,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int login_button_logIn=0x7f070004;
-        public static final int login_button_restartApplication=0x7f070005;
+        public static final int login_button_logIn=0x7f070006;
+        public static final int login_button_restartApplication=0x7f070007;
         public static final int login_editText_cardnumber=0x7f070003;
         public static final int login_editText_username=0x7f070001;
         public static final int login_textView_cardNumber=0x7f070002;
         public static final int login_textView_username=0x7f070000;
-        public static final int menu_settings=0x7f070006;
+        public static final int menu_settings=0x7f070008;
+        public static final int name_editText=0x7f070004;
+        public static final int number_editText=0x7f070005;
     }
     public static final class layout {
         public static final int activity_login_fragment=0x7f030000;
