3620104cc1885ebb165aa765d8491e21209a68d7
Fjoggis <fjoggis@gmail.com>
2012-11-07 19:50:54 +0100

    Added the new gameactivity that handles numberinput. Havne't tested.

--- Oving6/gen/no/hist/vegardfj/oving6/R.java
+++ Oving6/gen/no/hist/vegardfj/oving6/R.java
@@ -15,8 +15,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int game_editText=0x7f070001;
-        public static final int game_textView=0x7f070000;
+        public static final int answer_editText=0x7f070001;
+        public static final int info_textView=0x7f070000;
         public static final int login_button_logIn=0x7f070002;
         public static final int login_button_restartApplication=0x7f070003;
         public static final int menu_settings=0x7f070006;
--- Oving6/src/no/hist/vegardfj/oving6/GameActivity.java
+++ Oving6/src/no/hist/vegardfj/oving6/GameActivity.java
@@ -1,16 +1,24 @@
 package no.hist.vegardfj.oving6;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class GameActivity extends Activity {
-
+	private HttpWrapperThreaded network;
+	final static String TAG = "HttpActivity";
+	final static String urlToServer = "http://tomcat.stud.aitel.hist.no/studtomas/tallspill.jsp"; 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +32,9 @@ public class GameActivity extends Activity {
     }
     
     public void onClickSendAnswer(View v) throws ClientProtocolException, IOException {
+    	EditText nameEditText2 = (EditText) findViewById(R.id.answer_editText);
+    	List<BasicNameValuePair> valueList = new ArrayList<BasicNameValuePair>();
+        valueList.add(new BasicNameValuePair("kortnummer", nameEditText2.getText().toString()));
+    	network.runHttpRequestInThread(HttpWrapperThreaded.HttpRequestType.HTTP_GET, valueList);
     }
 }
