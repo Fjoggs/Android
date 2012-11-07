b52e51a05dee1685896edd6b262a844f95700015
Fjoggis <fjoggis@gmail.com>
2012-11-07 21:21:32 +0100

    Newest changes. Work please.

--- Oving6/src/no/hist/vegardfj/oving6/GameActivity.java
+++ Oving6/src/no/hist/vegardfj/oving6/GameActivity.java
@@ -8,6 +8,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,7 +18,8 @@ import android.widget.EditText;
 public class GameActivity extends Activity {
 	private HttpWrapperThreaded network;
 	final static String TAG = "HttpActivity";
-	final static String urlToServer = "http://tomcat.stud.aitel.hist.no/studtomas/tallspill.jsp"; 
+	final static String urlToServer = "http://tomcat.stud.aitel.hist.no/studtomas/tallspill.jsp";
+	private int tries = 0;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,16 @@ public class GameActivity extends Activity {
     }
     
     public void onClickSendAnswer(View v) throws ClientProtocolException, IOException {
-    	EditText nameEditText2 = (EditText) findViewById(R.id.answer_editText);
-    	List<BasicNameValuePair> valueList = new ArrayList<BasicNameValuePair>();
-        valueList.add(new BasicNameValuePair("kortnummer", nameEditText2.getText().toString()));
-    	network.runHttpRequestInThread(HttpWrapperThreaded.HttpRequestType.HTTP_GET, valueList);
+    	if(tries < 2) {
+        	EditText nameEditText2 = (EditText) findViewById(R.id.answer_editText);
+        	List<BasicNameValuePair> valueList = new ArrayList<BasicNameValuePair>();
+            valueList.add(new BasicNameValuePair("kortnummer", nameEditText2.getText().toString()));
+        	network.runHttpRequestInThread(HttpWrapperThreaded.HttpRequestType.HTTP_GET, valueList);
+        	tries++;
+    	} else {
+    		tries = 0;
+        	Intent myIntent = new Intent(GameActivity.this, HttpActivity.class);
+        	GameActivity.this.startActivity(myIntent);
+    	}
     }
 }
